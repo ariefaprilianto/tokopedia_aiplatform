f5fba8beae444aa911b7ccd181123963305d2a69
Arief Aprilianto <arief.aprilianto@tokopedia.com>
2023-08-09 14:18:05 +0700

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tokopedia_aiplatform',
-    version='0.1.2',
+    version='0.1.3',
     author='PT Tokopedia',
     author_email='engineering@tokopedia.com',
     description='Tokopedia AI Platform Package',
